dcb146eeb94195edb27f074236c35549bbe9e1d7
mcjovial <emmanuelelias455@gmail.com>
2021-11-22 13:36:13 +0100

    launches route, controller & model

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 const app = express();
+
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 app.use(cors({
     origin: 'http://localhost:3000'
@@ -11,7 +13,9 @@ app.use(cors({
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.use(launchesRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
