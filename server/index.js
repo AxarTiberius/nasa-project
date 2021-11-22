33a3c9c7a6f053081eba87a74be143aa3627feca
mcjovial <emmanuelelias455@gmail.com>
2021-11-22 05:06:52 +0100

    load planets data

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const http = require("http");
 const app = require("./src/app");
+const { loadPlanetsData } = require('./src/models/planets.model');
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-});
+async function startServer() {
+    await loadPlanetsData();
+
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
+}
+
+startServer();
