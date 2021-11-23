8fb17cc4bee238b7001c7412a3281892adb58ca4
mcjovial <emmanuelelias455@gmail.com>
2021-11-23 04:08:02 +0100

    launches abort api & endpoints

--- server/src/controllers/launches/launches.controller.js
+++ server/src/controllers/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const { getLaunches, addNewLaunch } = require('../../models/launches.model');
+const { getLaunches, addNewLaunch, existsLaunchWithId, abortLaunchById } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res){
     return res.status(200).json(getLaunches());
@@ -7,7 +7,7 @@ function httpGetAllLaunches(req, res){
 function httpAddNewLaunch(req, res){
     const launch = req.body;
     
-    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination){
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target){
         return res.status(400).json({
             error: 'Missing required launch property',
         });
@@ -24,7 +24,22 @@ function httpAddNewLaunch(req, res){
     return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res){
+    const launchId = Number(req.params.id);
+    // if launch doesn't exist
+    if (!existsLaunchWithId(launchId)) {
+        return res.status(404).json({
+            error: 'Launch not found',
+        });
+    }
+
+    // if launch does exist
+    const aborted = abortLaunchById(launchId);
+    return res.status(200).json(aborted);
+}
+
 module.exports = {
     httpGetAllLaunches,
     httpAddNewLaunch,
+    httpAbortLaunch,
 }
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const launchesRouter = express.Router();
-const { httpGetAllLaunches, httpAddNewLaunch } = require('../../controllers/launches/launches.controller');
+const { httpGetAllLaunches, httpAddNewLaunch, httpAbortLaunch } = require('../../controllers/launches/launches.controller');
 
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/', httpAddNewLaunch);
+launchesRouter.delete('/:id', httpAbortLaunch)
 
 module.exports = launchesRouter;
