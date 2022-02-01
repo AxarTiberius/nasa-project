150d280033b229c82594d908c8a07281681d7b06
Axar Tiberius <axar.tiberius@protonmail.com>
2022-01-31 17:17:30 -0800

    fix start script terminal issues, switch to fork of arwes

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-MONGO_URL=mongodb://localhost/nasa-project PORT=1341 npm run server &
-NODE_OPTIONS=--openssl-legacy-provider PORT=1339 npm run client &
-
+MONGO_URL=mongodb://localhost/nasa-project PORT=1341 npm run server >>server.log 2>&1 < /dev/null &
+NODE_OPTIONS=--openssl-legacy-provider PORT=1339 npm run client >>client.log 2>&1 < /dev/null &
+tail -f client.log
